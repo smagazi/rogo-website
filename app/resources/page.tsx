00ac54e5e76f8f5359e1017df2fa385dc19e33db
Sara Magaziotis-Ginori <smagazi@Saras-MacBook-Pro.local>
2024-12-15 01:17:58 -0800

    added presentation to resources slide. adjust scaling + link to a pdf of it later

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -11,6 +11,8 @@ export default function Page() {
 
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">add presentation and repo </h1>
 
-    </section>
+      {/* Embed Google Slides Presentation */}
+
+      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRXN33DHZPofJc4I-KFA2kSLBscoQ957g5krb7D2P8e16nwUEDXWrWXmQuAtkuYuLtK5Z4T-BMso7zx/embed?start=false&loop=false&delayms=5000" frameBorder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>    </section>
   )
 }
