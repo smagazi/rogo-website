edca3b3e048ac1f7179be3a9d5e53ae1e2151894
aaravkgoel <44481630+aaravkgoel@users.noreply.github.com>
2024-12-20 19:17:39 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Page() {
           {/* Add Goals Diagram */}
           <div className="mb-8">
             <Image
-              src="/images/block-diagram.png" // Adjust this path to where you store your image
+              src="/images/blocks_habitat.jpg" // Adjust this path to where you store your image
               alt="Project goals diagram showing three steps: 1. Does software process CAD model to instructions? 2. Robot capable of pick and place without AR tags? 3. Robot independently builds structure?"
               width={800} // Adjust based on your needs
               height={250} // Adjust based on your needs
